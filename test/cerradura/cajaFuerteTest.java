d40d892942d9c9ee295884b8ba5bc6f590718858
PatriciaMariela <pachuberna@gmail.com>
2021-10-01 19:19:53 -0300

    este es una prubea de commit con git bash

---
+++ test/cerradura/cajaFuerteTest.java
@@ -0,0 +1,14 @@
+package cerradura;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class cajaFuerteTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
